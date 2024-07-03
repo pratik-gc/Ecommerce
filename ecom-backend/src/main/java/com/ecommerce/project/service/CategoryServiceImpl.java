e8034fd186a4bf82a324ec6968bf9303d5fd0761
Pratik Chaudhari <pratikcg12@outlook.com>
2024-07-03 21:08:41 +0530

    added more Category endpoints along with Response Entity wrapper with proper status codes

--- ecom-backend/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ ecom-backend/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,10 +1,13 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -28,12 +31,29 @@ public class CategoryServiceImpl implements CategoryService{
         Category category = categories.stream()
                 .filter( c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
-                .orElse(null);
-
-        if(category == null)
-            return "Category Not Found";
-
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
         categories.remove(category);
         return "Category with CategotyId : "+ categoryId + " is deleted Successfully";
     }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter( c -> c.getCategoryId().equals(categoryId))
+                .findFirst();
+                //.get()
+                //We could have used .get() here itself, but if the category is NOT FOUND, then we will get NULL
+                //So, rather than getting NullPointerException, we will use Optional and
+                //then evalute the optional using if() block
+        if(optionalCategory.isPresent()){
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+        }
+        //NOTE: We could have utilized the same logic written in above deleteCategory()
+        //It was also another way of doing the same thing
+    }
+
 }
