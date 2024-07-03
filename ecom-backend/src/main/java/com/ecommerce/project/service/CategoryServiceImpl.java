e0486b61a33105e1295fa2029efff5eae05190c5
Pratik Chaudhari <pratikcg12@outlook.com>
2024-07-03 14:38:45 +0530

    intial backend

---
+++ ecom-backend/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createNewCategory(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter( c -> c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElse(null);
+
+        if(category == null)
+            return "Category Not Found";
+
+        categories.remove(category);
+        return "Category with CategotyId : "+ categoryId + " is deleted Successfully";
+    }
+}
