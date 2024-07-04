64195fe4f75cf1ca4c74d72b4796c85306490d45
Pratik Chaudhari <pratikcg12@outlook.com>
2024-07-04 13:06:42 +0530

    added Lombok and some validations with GlobalExceptionHandler class

---
+++ ecom-backend/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.ecommerce.project.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice //Specialized version of @ControllerAdvice & it is geared towards REST APIs
+                //It will intercept any exception that will be thrown by any controller in the application
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+                //It is used to define methods in our ExceptionHandler to handle a specific type of exception
+                //This exception is raised when @NotBlank & @Valid validations are not satisfied
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+        return new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
+        //If we just write "return response", then we will get user-friendly error message but the response status code wil be 200 OK.
+        //To avoid getting incorrect response status code, we will wrap our Map object into ResponsseEntity class to show appropriate status code.
+    }
+
+}
--- ecom-backend/src/main/java/com/ecommerce/project/model/Category.java
+++ ecom-backend/src/main/java/com/ecommerce/project/model/Category.java
@@ -4,41 +4,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //In the context of JPA, an Entity represents a table in a relational database
 //@Entity
 //@Entity(name = "<Your_Customized_Table_Name_Here>")
 @Entity(name = "categories")
+@Data //A shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, and @Setter on all non-final fields, and @RequiredArgsConstructor!
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotBlank
+    //@Min(3)
+    //@Max(10)
+    //@Size(min = 3, max = 10)
     private String categoryName;
-
-    //It is recommended that you have a default constructor if you are creating an Entity
-    public Category() {
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
 }
