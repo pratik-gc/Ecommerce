be7360292099700a8d1c6a03d686a6afecd42d3f
Pratik Chaudhari <pratikcg12@outlook.com>
2024-07-08 22:30:35 +0530

    added bidirectional relationships in User, Role, Address & Product models

--- ecom-backend/src/main/java/com/ecommerce/project/model/Address.java
+++ ecom-backend/src/main/java/com/ecommerce/project/model/Address.java
@@ -1,15 +1,61 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
 
+    @NotBlank
+    @Size(min = 5, message = "Street Name must contain atleast 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building Name must contain atleast 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 3, message = "City Name must contain atleast 3 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State Name must contain atleast 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country Name must contain atleast 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Pincode must contain atleast 6 characters")
+    private String pincode;
+
+    @ToString.Exclude //Excluding users from ToString() of the Address() class
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String buildingName, String city, String state, String country, String pincode) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pincode = pincode;
+    }
 }
--- ecom-backend/src/main/java/com/ecommerce/project/model/Product.java
+++ ecom-backend/src/main/java/com/ecommerce/project/model/Product.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,73 +36,9 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
-
-//    public Product() {
-//    }
-
-//    public Product(Long productId, String productName, String description, Integer quantity, double price, double specialPrice, Category category) {
-//        this.productId = productId;
-//        this.productName = productName;
-//        this.description = description;
-//        this.quantity = quantity;
-//        this.price = price;
-//        this.specialPrice = specialPrice;
-//        this.category = category;
-//    }
-//
-//    public Long getProductId() {
-//        return productId;
-//    }
-
-//    public void setProductId(Long productId) {
-//        this.productId = productId;
-//    }
-//
-//    public String getProductName() {
-//        return productName;
-//    }
-//
-//    public void setProductName(String productName) {
-//        this.productName = productName;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public Integer getQuantity() {
-//        return quantity;
-//    }
-//
-//    public void setQuantity(Integer quantity) {
-//        this.quantity = quantity;
-//    }
-//
-//    public double getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(double price) {
-//        this.price = price;
-//    }
-//
-//    public double getSpecialPrice() {
-//        return specialPrice;
-//    }
-//
-//    public void setSpecialPrice(double specialPrice) {
-//        this.specialPrice = specialPrice;
-//    }
-//
-//    public Category getCategory() {
-//        return category;
-//    }
-//
-//    public void setCategory(Category category) {
-//        this.category = category;
-//    }
+    //==============For Seller of the Product==================
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+    //=====================END=================================
 }
