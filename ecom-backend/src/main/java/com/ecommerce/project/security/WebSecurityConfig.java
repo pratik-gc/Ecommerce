5db2f955a42219597d62e5448dbc527ea04296a5
Pratik Chaudhari <pratikcg12@outlook.com>
2024-07-09 11:39:35 +0530

    added WebSecurityConfig, AuthenticationController, Signin & Signup Endpoints

---
+++ ecom-backend/src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -0,0 +1,95 @@
+package com.ecommerce.project.security;
+
+import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
+import com.ecommerce.project.security.jwt.AuthTokenFilter;
+import com.ecommerce.project.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+            //This bean is used to authenticate users and get their details from database
+            //This ensures that Spring Security retrieves the user info from our database using this custom method
+            //and not from something which is in-built/default
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                                                    //.requestMatchers("/v3/api-docs/**").permitAll()
+                                                    .requestMatchers("/api/admin/**").permitAll()
+                                                    .requestMatchers("/api/public/**").permitAll()
+                                                    .requestMatchers("/swagger-ui/**").permitAll()
+                                                    .requestMatchers("/api/test/**").permitAll()
+                                                    .requestMatchers("/images/**").permitAll()
+                                                    .anyRequest().authenticated()
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+                    //With the help of this bean, we are configuring global security settings to exclude
+                    //specified paths from security filter chain configurations
+
+        return (web -> web.ignoring().requestMatchers("/v2/api-docs",
+                "configuration/ui",
+                "/swagger-resources/**",
+                "configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"));
+    }
+}
